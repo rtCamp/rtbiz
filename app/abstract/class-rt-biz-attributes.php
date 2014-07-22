eee813a6a290660613b04a23c1a3434aba28fd73
Udit Desai <desaiuditd@gmail.com>
2014-07-22 17:27:33 +0530

    Attributes List in Contacts List Table

--- app/abstract/class-rt-biz-attributes.php
+++ app/abstract/class-rt-biz-attributes.php
@@ -25,6 +25,8 @@ if ( ! class_exists( 'Rt_Biz_Attributes' ) ) {
 
 		public function __construct() {
 			add_action( 'init', array( $this, 'init_attributes' ) );
+			add_filter( 'rt_entity_columns', array( $this, 'attributes_columns' ), 10, 2 );
+			add_action( 'rt_entity_manage_columns', array( $this, 'manage_attributes_columns' ), 10, 3 );
 		}
 
 		function init_attributes() {
@@ -42,6 +44,43 @@ if ( ! class_exists( 'Rt_Biz_Attributes' ) ) {
 			);
 
 			$rt_biz_rt_attributes->add_attributes_page( $this->attributes_page_slug, Rt_Biz::$dashboard_slug, '', $admin_cap, $terms_caps, $render_type = false, $storage_type = false, $orderby = false );
+		}
+
+		function attributes_columns( $columns, $entity_obj ) {
+			$rt_biz_attributes_model = new RT_Attributes_Model();
+			$rt_biz_attributes_relationship_model = new RT_Attributes_Relationship_Model();
+			$relations = $rt_biz_attributes_relationship_model->get_relations_by_post_type( $entity_obj->post_type );
+			foreach ( $relations as $r ) {
+				$attr = $rt_biz_attributes_model->get_attribute( $r->attr_id );
+				if ( $attr->attribute_store_as == 'taxonomy' ) {
+					$columns[ $attr->attribute_name ] = $attr->attribute_label;
+				}
+			}
+			return $columns;
+		}
+
+		function manage_attributes_columns( $column, $post_id, $entity_obj ) {
+			global $rt_biz_rt_attributes;
+			$rt_biz_attributes_model = new RT_Attributes_Model();
+			$rt_biz_attributes_relationship_model = new RT_Attributes_Relationship_Model();
+			$relations = $rt_biz_attributes_relationship_model->get_relations_by_post_type( $entity_obj->post_type );
+			foreach ( $relations as $r ) {
+				$attr = $rt_biz_attributes_model->get_attribute( $r->attr_id );
+				if ( $attr->attribute_store_as == 'taxonomy' && $attr->attribute_name == $column ) {
+					$terms = wp_get_post_terms( $post_id, $rt_biz_rt_attributes->get_taxonomy_name( $attr->attribute_name ) );
+					$links = array();
+					foreach ( $terms as $t ) {
+						$links[] = '<a href="' . add_query_arg(array( 'taxonomy' => $rt_biz_rt_attributes->get_taxonomy_name( $attr->attribute_name ), 'term' => $t->slug )) . '">' . $t->name . '</a>';
+					}
+
+					if ( ! empty( $links ) ) {
+						echo implode( ' , ', $links );
+					} else {
+						echo '-';
+					}
+					break;
+				}
+			}
 		}
 
 	}
