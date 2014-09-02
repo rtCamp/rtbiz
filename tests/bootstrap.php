f27eb5e4d11ef93d40a5c684179e232d56fd32d3
Udit Desai <desaiuditd@gmail.com>
2014-09-02 21:22:14 +0530

    Update Travis CI Config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,22 +6,30 @@
  *
  * @package wordpress-plugin-tests
  */
+echo "Welcome to the rtLib Test Suite" . PHP_EOL;
+echo "Version: 1.0" . PHP_EOL . PHP_EOL;
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = realpath( dirname( __FILE__ ) . '/../tmp-wp-dev/wordpress-tests' ) . '/';
+}
+
+echo 'Tests folder: ' . $_tests_dir . PHP_EOL . PHP_EOL;
+
+// load some helpful functions
+require_once $_tests_dir . 'functions.php';
+
+function _rt_lib_manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/wp-helpers.php';
+}
+
+tests_add_filter(
+	'muplugins_loaded', '_rt_lib_manually_load_plugin'
+);
+
 
 // Activates this plugin in WordPress so it can be tested.
 $GLOBALS[ 'wp_tests_options' ] = array( 'active_plugins' => array( 'wp-helpers/wp-helpers.php' ), );
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir || ! file_exists( $_tests_dir ) ) {
-	$_tests_dir = '/Users/faishal/work/wordpress-develop/tests/phpunit';
-}
 
-require_once $_tests_dir . '/includes/functions.php';
-
-
-tests_add_filter(
-	'muplugins_loaded', function () {
-		require dirname( dirname( __FILE__ ) ) . '/wp-helpers.php';
-	}
-);
-
-require $_tests_dir . '/includes/bootstrap.php';
+require_once $_tests_dir . 'bootstrap.php';
 require_once 'rt-wp-test-case.php';
