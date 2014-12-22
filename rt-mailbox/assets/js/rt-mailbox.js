1b4bdbd4b72c1c96e5ff724d19df308746f5828d
Dipesh Kakadiya <dipesh.kakadiya@rtcamp.com>
2014-12-22 13:41:13 +0530

    Squashed 'app/lib/' changes from 0def2b3..82636a5
    82636a5 Merge pull request #45 from dipskakadiya/master
    f41413d Merge branch 'develop'
    6217525 Merge pull request #44 from dipskakadiya/develop
    22303e4 phpcs fixed
    4c1c5df phpcs fixed
    f9f7635 phpcs fixed
    a7c2cd0 rtMailBox box ui changes & Classname & elementId and elementName changes
    c77fe77 Merge branch 'develop'
    d98c7a3 Merge branch 'develop'
    4e3c9c1 Merge branch 'develop'
    d312d02 Merge branch 'develop'
    6b914ab Merge pull request #24 from desaiuditd/master
    
    git-subtree-dir: app/lib
    git-subtree-split: 82636a5c2abd6ab75a3ae7fbc4f5da777d5aa146

--- rt-mailbox/assets/js/rt-mailbox.js
+++ rt-mailbox/assets/js/rt-mailbox.js
@@ -2,46 +2,46 @@ jQuery( document ).ready(function(){
 	jQuery( document ).on( 'click', '.rthd-edit-server', function ( e ) {
 		e.preventDefault();
 		var server_id = jQuery( this ).data( 'server-id' );
-		jQuery( '#rthd_imap_server_' + server_id ).toggleClass( 'rthd-hide-row' ).toggleClass( 'rthd-show-row' );
+		jQuery( '#rtmailbox_imap_server_' + server_id ).toggleClass( 'rtmailbox-hide-row' ).toggleClass( 'rtmailbox-show-row' );
 	} );
-	jQuery( document ).on( 'click', '#rthd_add_imap_server', function ( e ) {
+	jQuery( document ).on( 'click', '#rtmailbox_add_imap_server', function ( e ) {
 		e.preventDefault();
-		jQuery( '#rthd_new_imap_server' ).toggleClass( 'rthd-hide-row' ).toggleClass( 'rthd-show-row' );
+		jQuery( '#rthd_new_imap_server' ).toggleClass( 'rtmailbox-hide-row' ).toggleClass( 'rtmailbox-show-row' );
 	} );
-	jQuery( document ).on( 'click', '.rthd-remove-server', function ( e ) {
+	jQuery( document ).on( 'click', '.rtmailbox-remove-server', function ( e ) {
 		e.preventDefault();
 		var flag = confirm( 'Are you sure you want to remove this server ?' );
 		var server_id = jQuery( this ).data( 'server-id' );
 		if ( flag ) {
-			jQuery( '#rthd_imap_server_' + server_id ).remove();
+			jQuery( '#rtmailbox_imap_server_' + server_id ).remove();
 			jQuery( this ).parent().parent().remove();
 		}
 	} );
-	jQuery( document ).on( 'click', '#rthd_add_personal_email', function ( e ) {
+	jQuery( document ).on( 'click', '#rtmailbox_add_personal_email', function ( e ) {
 		e.preventDefault();
-		jQuery( '#rthd_email_acc_type_container' ).toggleClass( 'rthd-hide-row' ).toggleClass( 'rthd-show-row' );
-		if ( jQuery( '#rthd_email_acc_type_container' ).hasClass( 'rthd-hide-row' ) ) {
-			jQuery( '#rthd_goauth_container' ).removeClass( 'rthd-show-row' ).addClass( 'rthd-hide-row' );
-			jQuery( '#rthd_add_imap_acc_form' ).removeClass( 'rthd-show-row' ).addClass( 'rthd-hide-row' );
-			jQuery( '#rthd_select_email_acc_type' ).val( '' ).change();
+		jQuery( '#rtmailbox_email_acc_type_container' ).toggleClass( 'rtmailbox-hide-row' ).toggleClass( 'rtmailbox-show-row' );
+		if ( jQuery( '#rtmailbox_email_acc_type_container' ).hasClass( 'rtmailbox-hide-row' ) ) {
+			jQuery( '#rthd_goauth_container' ).removeClass( 'rtmailbox-show-row' ).addClass( 'rtmailbox-hide-row' );
+			jQuery( '#rtmailbox_add_imap_acc_form' ).removeClass( 'rtmailbox-show-row' ).addClass( 'rtmailbox-hide-row' );
+			jQuery( '#rtmailbox_select_email_acc_type' ).val( '' ).change();
 		}
 	} );
-	jQuery( document ).on( 'change', '#rthd_select_email_acc_type', function () {
+	jQuery( document ).on( 'change', '#rtmailbox_select_email_acc_type', function () {
 		if ( jQuery( this ).val() === 'goauth' ) {
-			jQuery( '#rthd_goauth_container' ).removeClass( 'rthd-hide-row' ).addClass( 'rthd-show-row' );
-			jQuery( '#rthd_add_imap_acc_form' ).removeClass( 'rthd-show-row' ).addClass( 'rthd-hide-row' );
-			jQuery( '#rthd_add_imap_acc_form input[type=email]' ).remove();
-			jQuery( '#rthd_add_imap_acc_form input[type=password]' ).remove();
+			jQuery( '#rthd_goauth_container' ).removeClass( 'rtmailbox-hide-row' ).addClass( 'rtmailbox-show-row' );
+			jQuery( '#rtmailbox_add_imap_acc_form' ).removeClass( 'rtmailbox-show-row' ).addClass( 'rtmailbox-hide-row' );
+			jQuery( '#rtmailbox_add_imap_acc_form #rtmailbox_add_imap_acc_fields input[type=email]' ).remove();
+			jQuery( '#rtmailbox_add_imap_acc_form #rtmailbox_add_imap_acc_fields input[type=password]' ).remove();
 		} else if ( jQuery( this ).val() === 'imap' ) {
-			jQuery( '#rthd_add_imap_acc_form' ).removeClass( 'rthd-hide-row' ).addClass( 'rthd-show-row' );
-			jQuery( '#rthd_goauth_container' ).removeClass( 'rthd-show-row' ).addClass( 'rthd-hide-row' );
-			jQuery( '#rthd_add_imap_acc_form' ).append( '<input type="email" autocomplete="off" name="rthd_imap_user_email" placeholder="Email"/>' );
-			jQuery( '#rthd_add_imap_acc_form' ).append( '<input type="password" autocomplete="off" name="rthd_imap_user_pwd" placeholder="Password"/>' );
+			jQuery( '#rtmailbox_add_imap_acc_form' ).removeClass( 'rtmailbox-hide-row' ).addClass( 'rtmailbox-show-row' );
+			jQuery( '#rthd_goauth_container' ).removeClass( 'rtmailbox-show-row' ).addClass( 'rtmailbox-hide-row' );
+			jQuery( '#rtmailbox_add_imap_acc_form #rtmailbox_add_imap_acc_fields' ).append( '<input type="email" autocomplete="off" name="rtmailbox_imap_user_email" placeholder="Email"/>' );
+			jQuery( '#rtmailbox_add_imap_acc_form #rtmailbox_add_imap_acc_fields' ).append( '<input type="password" autocomplete="off" name="rtmailbox_imap_user_pwd" placeholder="Password"/>' );
 		} else {
-			jQuery( '#rthd_goauth_container' ).removeClass( 'rthd-show-row' ).addClass( 'rthd-hide-row' );
-			jQuery( '#rthd_add_imap_acc_form' ).removeClass( 'rthd-show-row' ).addClass( 'rthd-hide-row' );
-			jQuery( '#rthd_add_imap_acc_form input[type=email]' ).remove();
-			jQuery( '#rthd_add_imap_acc_form input[type=password]' ).remove();
+			jQuery( '#rthd_goauth_container' ).removeClass( 'rtmailbox-show-row' ).addClass( 'rtmailbox-hide-row' );
+			jQuery( '#rtmailbox_add_imap_acc_form' ).removeClass( 'rtmailbox-show-row' ).addClass( 'rtmailbox-hide-row' );
+			jQuery( '#rtmailbox_add_imap_acc_form #rtmailbox_add_imap_acc_fields input[type=email]' ).remove();
+			jQuery( '#rtmailbox_add_imap_acc_form #rtmailbox_add_imap_acc_fields input[type=password]' ).remove();
 		}
 	});
 
@@ -57,7 +57,7 @@ jQuery( document ).ready(function(){
 
 	jQuery( '.rtMailbox-hide-mail-folders' ).click( function ( e ) {
 		e.preventDefault();
-		jQuery( this ).parent().parent().next( 'tr' ).toggleClass( 'rthd-hide-row' );
+		jQuery( this ).parent().next( 'table' ).toggleClass( 'rtmailbox-hide-row' );
 	});
 
 });
