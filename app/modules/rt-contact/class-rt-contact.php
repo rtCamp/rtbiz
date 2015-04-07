e96ec9556fd0c727ab7dd989a9357df6900383a6
Dipesh Kakadiya <dipesh.kakadiya@rtcamp.com>
2015-04-07 20:28:52 +0530

    acl metabox issue fixed

--- app/modules/rt-contact/class-rt-contact.php
+++ app/modules/rt-contact/class-rt-contact.php
@@ -86,6 +86,9 @@ if ( ! class_exists( 'Rt_Contact' ) ) {
 			 * Add ACL meta box
 			 */
 			global $rt_access_control;
+			if ( ! empty( $_REQUEST[ 'post' ] ) ){
+				$_REQUEST['post_type'] = get_post_type( $_REQUEST[ 'post' ] );
+			}
 			if ( ! empty( $_REQUEST['post_type'] ) && $_REQUEST['post_type'] = $this->post_type && current_user_can( 'create_users' ) ){
 				add_action( 'rt_biz_entity_meta_boxes', array( $this, 'contact_meta_boxes' ) );
 				add_action( 'rt_biz_save_entity_meta', array( $rt_access_control, 'save_profile_level_permission' ) );
