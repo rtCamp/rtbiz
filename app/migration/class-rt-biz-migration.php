1e0276c3b90bb2c4a56f5f6cb884a9285dab4efe
Dipesh Kakadiya <dipesh.kakadiya@rtcamp.com>
2015-04-14 11:58:28 +0530

    migration class added

---
+++ app/migration/class-rt-biz-migration.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Don't load this file directly!
+ */
+if ( ! defined( 'ABSPATH' ) ){
+	exit;
+}
+
+if ( ! class_exists( 'Rt_Biz_Migration' ) ) {
+
+	/*
+	 *
+	 */
+	class Rt_Biz_Migration {
+
+		/*
+		 * construct
+		 */
+		public function __construct() {
+			$this->init();
+		}
+
+		/*
+		 * load current migration class
+		 */
+		public function init() {
+			if ( strcmp (RT_BIZ_VERSION , '1.2.12') < 1 ){
+				$acl_migration = new Rt_Biz_ACL_Migration();
+			}
+		}
+	}
+}
