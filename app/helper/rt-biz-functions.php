c0c482e0be58bc21a946aba85bc9e001efbf4689
Utkarsh Patel <utkarsh.patel@rtcamp.com>
2015-04-24 13:40:13 +0530

    product sync warning fix

--- app/helper/rt-biz-functions.php
+++ app/helper/rt-biz-functions.php
@@ -654,9 +654,11 @@ function rt_biz_set_redux_setting( $key, $val ){
 function rt_biz_get_offering_selection_setting(){
 	$return  = array();
 	$redux = rt_biz_get_redux_settings();
-	foreach ( $redux['offering_plugin'] as $key => $val ){
-		if ( ! empty( $val ) ){
-			$return[] = $key;
+	if ( ! empty( $redux['offering_plugin'] ) ) {
+		foreach ( $redux['offering_plugin'] as $key => $val ) {
+			if ( ! empty( $val ) ) {
+				$return[] = $key;
+			}
 		}
 	}
 	return $return;
