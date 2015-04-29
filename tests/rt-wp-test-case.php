5f670142303e09bee8bc5f1278718e184dd21a00
dipeshkakadiya <dipesh.kakadiya@rtcamp.com>
2015-04-29 18:09:52 +0530

    Squashed 'app/lib/' changes from fbf84ff..68bf569
    68bf569 js validation added
    
    git-subtree-dir: app/lib
    git-subtree-split: 68bf5693a56f84372c7a58bc1607a4bcda70ea03

--- tests/rt-wp-test-case.php
+++ tests/rt-wp-test-case.php
@@ -6,13 +6,14 @@
  * Date: 18/02/14
  * Time: 4:30 PM
  */
-class RT_WP_TestCase extends WP_UnitTestCase {
-
+class RT_WP_TestCase extends WP_UnitTestCase
+{
 	/**
 	 * Ensure that the plugin has been installed and activated.
 	 */
-	function test_plugin_activated() {
-		$this->assertTrue( is_plugin_active( 'rtbiz/index.php' ) );
+	function test_plugin_activated()
+	{
+		$this->assertTrue( is_plugin_active( 'rt-lib/rt-lib.php' ) );
 	}
 
 }
