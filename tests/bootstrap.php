cf369ce5d65267bcbe34d4d4c86382daf1bfd662
dipeshkakadiya <dipesh.kakadiya@rtcamp.com>
2015-05-15 23:37:10 +0530

    update bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,10 +7,10 @@ $GLOBALS['wp_tests_options'] = array(
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = '/tmp/wordpress-tests-lib/';
 }
 
-require_once $_tests_dir . '/includes/functions.php';
+require_once $_tests_dir . 'includes/functions.php';
 
 function _manually_load_plugin() {
 	define( 'WP_ADMIN', true );
