3d8a3342d3664e53bd1504dacfcecfbcd14c2e2c
vishal <vishalkakadiya123@gmail.com>
2015-08-06 13:57:15 +0530

    Revert the module param

--- admin/classes/rtbiz-teams/class-rtbiz-teams.php
+++ admin/classes/rtbiz-teams/class-rtbiz-teams.php
@@ -216,7 +216,7 @@ if ( ! class_exists( 'Rtbiz_Teams' ) ) {
 				case 'contacts':
 					$term = get_term( $term_id, self::$slug );
 					$contacts_count = count( rtbiz_get_team_contacts( $term_id ) );
-					echo '<a href="' . esc_url( admin_url( 'edit.php?post_type='.rtbiz_get_contact_post_type().'&contact_group=staff&module=rtbiz-helpdesk&' . self::$slug . '=' . $term->slug ) ) . '">' . $contacts_count . '</a>';
+					echo '<a href="' . esc_url( admin_url( 'edit.php?post_type='.rtbiz_get_contact_post_type().'&contact_group=staff&' . self::$slug . '=' . $term->slug ) ) . '">' . $contacts_count . '</a>';
 					break;
 				case 'email_address';
 					$email_address = $this->get_team_meta( 'email_address', $term_id );
