f8c467583d30a0fc5e71b3408c74d228f2340090
krishana7911 <krishanagautam7911@gmail.com>
2025-02-25 15:35:00 +0530

    update packages and add nvmrc

--- gruntfile.js
+++ gruntfile.js
@@ -1,92 +1,90 @@
 'use strict';
-const sass = require('node-sass');
+const sass = require('sass');
 
-module.exports = function ( grunt ) {
+module.exports = function (grunt) {
 
-	// load all grunt tasks matching the `grunt-*` pattern
-	// Ref. https://npmjs.org/package/load-grunt-tasks
-	require( 'load-grunt-tasks' )( grunt );
+    // Load all grunt tasks matching the `grunt-*` pattern
+    require('load-grunt-tasks')(grunt);
 
-	grunt.initConfig( {
-		// watch for changes and trigger sass, jshint, uglify and livereload
-		watch: {
-			sass: {
-				files: [ 'admin/css/**/*.{scss,sass}' ],
-				tasks: [ 'sass', 'autoprefixer', 'cssmin' ]
-			},
-			js: {
-				files: '<%= jshint.all %>',
-				tasks: [ 'jshint', 'uglify' ]
-			}
-		},
-		// sass
-		sass: {
-			dist: {
-				options: {
-					implementation: sass,
-					style: 'expanded'
-				},
-				files: {
-					'admin/css/biz-admin.css': 'admin/css/scss/biz-admin.scss'
-				}
-			}
-		},
-		// autoprefixer
-		autoprefixer: {
-			options: {
-				browsers: [ 'last 2 versions', 'ie 9', 'ios 6', 'android 4' ],
-				map: true
-			},
-			files: {
-				expand: true,
-				flatten: true,
-				src: 'admin/css/*.css',
-				dest: 'admin/css'
-			}
-		},
-		// css minify
-		cssmin: {
-			options: {
-				keepSpecialComments: 1
-			},
-			minify: {
-				expand: true,
-				cwd: 'admin/css',
-				src: [ '*.css', '!*.min.css' ],
-				dest: 'admin/css',
-				ext: '.min.css'
-			}
-		},
-		// javascript linting with jshint
-		jshint: {
-			options: {
-				jshintrc: '.jshintrc',
-				"force": true
-			},
-			all: [
-				'gruntfile.js',
-				'admin/js/**/*.js'
-			]
-		},
-		// uglify to concat, minify, and make source maps
-		uglify: {
-			admin: {
-				options: {
-					sourceMap: 'admin/js/rtbiz-main.js.map',
-					sourceMappingURL: 'rtbiz-main.js.map',
-					sourceMapPrefix: 2
-				},
-				files: {
-					'admin/js/rtbiz-main.min.js': [
-						'admin/js/admin.js',
-						'admin/js/rtbiz-plugin-check.js',
-						'admin/js/validation.js'
-					]
-				}
-			}
-		}
-	} );
+    grunt.initConfig({
+        watch: {
+            sass: {
+                files: ['admin/css/**/*.{scss,sass}'],
+                tasks: ['sass', 'postcss', 'cssmin']
+            },
+            js: {
+                files: ['gruntfile.js', 'admin/js/**/*.js'],
+                tasks: ['jshint', 'uglify']
+            }
+        },
+        // Sass task
+        sass: {
+            dist: {
+                options: {
+                    implementation: sass,
+                    style: 'expanded'
+                },
+                files: {
+                    'admin/css/biz-admin.css': 'admin/css/scss/biz-admin.scss'
+                }
+            }
+        },
+        // PostCSS task
+        postcss: {
+            options: {
+                map: false,
+                plugins: [
+                    require('autoprefixer')({
+                        overrideBrowserslist: ['last 2 versions']
+                    })
+                ]
+            },
+            dist: {
+                src: 'admin/css/*.css'
+            }
+        },
+        // Minify CSS
+        cssmin: {
+            options: {
+                keepSpecialComments: 1
+            },
+            minify: {
+                expand: true,
+                cwd: 'admin/css',
+                src: ['*.css', '!*.min.css'],
+                dest: 'admin/css',
+                ext: '.min.css'
+            }
+        },
+        // JavaScript linting with jshint
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                force: true
+            },
+            all: ['gruntfile.js', 'admin/js/**/*.js']
+        },
+        // Uglify JavaScript (minify and create source maps)
+        uglify: {
+            admin: {
+                options: {
+                    sourceMap: true,
+                    sourceMapName: 'admin/js/rtbiz-main.js.map'
+                },
+                files: {
+                    'admin/js/rtbiz-main.min.js': [
+                        'admin/js/admin.js',
+                        'admin/js/rtbiz-plugin-check.js',
+                        'admin/js/validation.js'
+                    ]
+                }
+            }
+        }
+    });
 
-	// register task
-	grunt.registerTask( 'default', [ 'sass', 'autoprefixer', 'cssmin', 'uglify', 'watch' ] );
+    // Register default task (for development)
+    grunt.registerTask('default', ['sass', 'postcss', 'cssmin', 'uglify', 'watch']);
+
+    // Register build task (for production)
+    grunt.registerTask('build', ['sass', 'postcss', 'cssmin', 'uglify']);
 };
