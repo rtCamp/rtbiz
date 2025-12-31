c60e6b1f5363ae00fd5db801752000630a41a6ac
Sagar Tamang <mi5t4n@gmail.com>
2025-12-31 14:37:41 +0545

    Fix: DOM text reinterpreted as HTML in redux-framework (#137)

--- vendor/redux/redux-core/inc/fields/select_image/redux-select-image.js
+++ vendor/redux/redux-core/inc/fields/select_image/redux-select-image.js
@@ -3,6 +3,44 @@
 (function( $ ) {
 	'use strict';
 
+	// Simple URL sanitizer for image src attributes.
+	// Allows http(s), protocol-relative URLs, and root / relative paths.
+	function sanitizeImageSrc( src ) {
+		if ( 'string' !== typeof src ) {
+			return '';
+		}
+
+		src = src.trim();
+
+		if ( '' === src ) {
+			return '';
+		}
+
+		// Disallow javascript:, data:, and other dangerous schemes.
+		var lower = src.toLowerCase();
+		if ( lower.indexOf( 'javascript:' ) === 0 || lower.indexOf( 'data:' ) === 0 ) {
+			return '';
+		}
+
+		// Allow absolute http/https URLs.
+		if ( lower.indexOf( 'http://' ) === 0 || lower.indexOf( 'https://' ) === 0 ) {
+			return src;
+		}
+
+		// Allow protocol-relative URLs.
+		if ( src.indexOf( '//' ) === 0 ) {
+			return src;
+		}
+
+		// Allow root-relative and simple relative paths.
+		if ( src.indexOf( '/' ) === 0 || src.indexOf( './' ) === 0 || src.indexOf( '../' ) === 0 ) {
+			return src;
+		}
+
+		// Fallback: treat as relative path segment.
+		return src;
+	}
+
 	redux.field_objects              = redux.field_objects || {};
 	redux.field_objects.select_image = redux.field_objects.select_image || {};
 
@@ -36,7 +74,7 @@
 				value   = el.find( 'select.redux-select-images' ).val();
 				preview = el.find( 'select.redux-select-images' ).parents( '.redux-field:first' ).find( '.redux-preview-image' );
 
-				preview.attr( 'src', value );
+				preview.attr( 'src', sanitizeImageSrc( value ) );
 
 				el.find( '.redux-select-images' ).on(
 					'change',
@@ -51,7 +89,7 @@
 								}
 							);
 						} else {
-							preview.attr( 'src', $( this ).val() );
+							preview.attr( 'src', sanitizeImageSrc( $( this ).val() ) );
 							preview.fadeIn().css( 'visibility', 'visible' );
 						}
 					}
